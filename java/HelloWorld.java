19b327dad8c855b56f8b02cd7de324d22a622c47
lkhmxj <opsliao@163.com>
2020-07-28 17:08:34 +0800

    java

---
+++ java/HelloWorld.java
@@ -0,0 +1,10 @@
+package test.java.com.blockdate.api;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
